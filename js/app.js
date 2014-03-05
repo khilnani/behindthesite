71427f830223772a870a7ecd663bfa1f87917095
Nik <nik@khilnani.org>
2014-03-04 20:15:33 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,9 +2,12 @@ $(function () {
   var oTable = $('#example').dataTable( {
 		"bProcessing": true,
 		"sAjaxSource": 'data/trends.json',
-		"sScrollX": "100%",
 		"bScrollCollapse": true,
 		"bPaginate": false
 	} );
-    new FixedColumns( oTable );
+    new FixedColumns( oTable, {
+	"iLeftColumns": 1,
+    	"iRightColumns": 0
+    	
+    } );
 });
