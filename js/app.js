5bb7c401260dda82fdd4d3aa8a04fd28b84134ed
Nik <nik@khilnani.org>
2014-03-04 20:05:06 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ $(function () {
   var oTable = $('#example').dataTable( {
 		"bProcessing": true,
 		"sScrollX": "100%",
+		"sScrollXInner": "150%",
         	"bScrollCollapse": true,
-		"bPaginate": false,
 		"sAjaxSource": 'data/trends.json'
 	} );
     new FixedColumns( oTable );
