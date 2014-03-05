efe37e83bf380e5153c0cc47d057894704dc3cee
Nik <nik@khilnani.org>
2014-03-04 20:21:38 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(function () {
   var oTable = $('#example').dataTable( {
 		"bProcessing": true,
 		"sAjaxSource": 'data/trends.json',
-		"sScrollX": "100%",
+		"sScrollX": "100",
 		"sScrollXInner": "100%",
 		"bScrollCollapse": true,
 		"bPaginate": false
