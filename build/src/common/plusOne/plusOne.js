3ff1bd821727a1822c5bf5e62237a10376daea2e
khilnani <nik@khilnani.org>
2014-10-18 20:44:49 -0400

    updated

--- build/src/common/plusOne/plusOne.js
+++ build/src/common/plusOne/plusOne.js
@@ -1,15 +1,11 @@
-angular.module( 'plusOne', [] )
-
-.directive( 'plusOne', function() {
+angular.module('plusOne', []).directive('plusOne', function () {
   return {
-    link: function( scope, element, attrs ) {
-      gapi.plusone.render( element[0], {
-        "size": "medium",
-        "href": "http://bit.ly/ngBoilerplate"
+    link: function (scope, element, attrs) {
+      gapi.plusone.render(element[0], {
+        'size': 'medium',
+        'href': 'http://bit.ly/ngBoilerplate'
       });
     }
   };
-})
-
+});
 ;
-
