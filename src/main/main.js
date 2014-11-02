f1161c66b6ae423ea0b24786a4304bd0b19f2258
Nik <nik@khilnani.org>
2014-11-02 17:55:45 -0500

    Update main.js

--- src/main/main.js
+++ src/main/main.js
@@ -90,7 +90,7 @@ function popover ($templateCache, $compile) {
     var popupConfig = {
       html: true,
       title: attrs.title,
-      content: attrs.content,
+      content: marked( attrs.content ),
       placement: 'top',
       template: tmpl,
       trigger: 'manual'
@@ -140,13 +140,13 @@ function product ($templateCache, $compile) {
     scope.twitter = attrs.twitter;
     scope.irc = attrs.irc;
     scope.blogs = attrs.blogs;
-    scope.description = attrs.description;
+    scope.description = marked( attrs.description );
     scope.repo = attrs.repo;
     scope.issues = attrs.issues;
     scope.docs = attrs.docs;
     scope.category = attrs.category;
     scope.categorypath = attrs.categorypath;
-    scope.notes = attrs.notes;
+    scope.notes = marked( attrs.notes );
     // https://docs.angularjs.org/api/ng/service/$compile
     var contentHtml = $compile($templateCache.get('productHoverContentTmpl.html'))(scope);
 
@@ -315,9 +315,9 @@ function MainCtrl($scope, $timeout, Common, TaxonomySvc, StackSvc) {
           issues: product.issues,
           docs: product.docs,
           updated: product.stack.updated,
-          insight: marked( product.stack.insight ),
-          notes: marked( product.stack.notes ),
-          references: marked( product.stack.references )
+          insight: product.stack.insight,
+          notes: product.stack.notes,
+          references: product.stack.references
         };
         model.company = product.company;
         model.tiers = [];
@@ -328,9 +328,6 @@ function MainCtrl($scope, $timeout, Common, TaxonomySvc, StackSvc) {
         var tiers = product.stack.tiers;
         for(var tier in tiers) {
           var index = vm.findHeaderIndex( tiers[tier] );
-          if(tiers[tier].notes) {
-            tiers[tier].notes = marked( tiers[tier].notes );
-          }
 //          console.log('index: ' + index);
 //          console.log(tiers[tier].product);
           model.tiers[index].push( tiers[tier] )
