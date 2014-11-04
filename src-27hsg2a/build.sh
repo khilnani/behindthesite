7b9cdfceea99fbf07619d58ed462f28ce616cd8e
Nik <nik@khilnani.org>
2014-11-03 20:20:41 -0500

    Update build.sh

--- src-27hsg2a/build.sh
+++ src-27hsg2a/build.sh
@@ -4,15 +4,16 @@ ng-annotate -ar app.js -o app.js --single_quotes
 ng-annotate -ar main/main.js -o main/main.js --single_quotes
 
 uglifyjs ../vendor/marked/marked.js \
-  ../vendor/jquery/dist/jquery.min.js \
-  ../vendor/bootstrap/dist/js/bootstrap.min.js \
-  ../vendor/angular/angular.min.js \
-  ../vendor/angular-resource/angular-resource.min.js \
-  ../vendor/angular-route/angular-route.min.js \
-  ../vendor/angular-animate/angular-animate.min.js \
-  ../vendor/angular-sanitize/angular-sanitize.min.js \
-  ../vendor/angular-loading-bar/build/loading-bar.min.js \
-  ../vendor/ngInfiniteScroll/build/ng-infinite-scroll.min.js \
+  ../vendor/jquery/dist/jquery.js \
+  ../vendor/bootstrap/dist/js/bootstrap.js \
+  ../vendor/angular/angular.js \
+  ../vendor/angular-resource/angular-resource.js \
+  ../vendor/angular-route/angular-route.js \
+  ../vendor/angular-animate/angular-animate.js \
+  ../vendor/angular-sanitize/angular-sanitize.js \
+  ../vendor/angular-loading-bar/build/loading-bar.js \
+  ../vendor/ngInfiniteScroll/build/ng-infinite-scroll.js \
+  common/aes.js \
   app.js \
   main/main.js \
   -o ../_/js/min.js \
