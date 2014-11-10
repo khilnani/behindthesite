4cff7dd9bbcd0153ae04d25009f3f1132e750ddb
Nik <nik@khilnani.org>
2014-11-10 12:23:05 -0500

    Update main.js

--- src-27hsg2a/main/main.js
+++ src-27hsg2a/main/main.js
@@ -100,7 +100,7 @@ angular.module('bts.directives', [])
     ele.affix({
       offset: {
         top: function() { 
-          return  $('#main').offset().top - 80;
+          return  $('#main').offset().top - 100;
         }
       }
     })
