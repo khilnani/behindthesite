b35b526af109cc6ff647dff241f5bd61ba9b074f
Nik <nik@khilnani.org>
2014-11-11 14:45:13 -0500

    Update app.js

--- src-27hsg2a/app.js
+++ src-27hsg2a/app.js
@@ -117,3 +117,20 @@ bts.factory('Common', ['$location', '$http', function ($location, $http) {
 bts.run(function() {
     console.log('run()');
 });
+
+/**
+ * HACK Do not reload the current template if it is not needed.
+ *
+ * See AngularJS: Change hash and route without completely reloading controller http://stackoverflow.com/questions/12115259/angularjs-change-hash-and-route-without-completely-reloading-controller
+ */
+bts.factory('DoNotReloadCurrentTemplate', ['$route', function($route) {
+  return function(scope) {
+    var lastRoute = $route.current;
+    scope.$on('$locationChangeSuccess', function() {
+      if (lastRoute.$$route.templateUrl === $route.current.$$route.templateUrl) {
+        console.log('DoNotReloadCurrentTemplate not reloading template: ' + $route.current.$$route.templateUrl);
+        $route.current = lastRoute;
+      }
+    });
+  };
+}]);
