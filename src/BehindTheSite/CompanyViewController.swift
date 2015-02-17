6463e9aaa69931678fa147e765fef3c0048d75d3
khilnani <nik@khilnani.org>
2015-02-16 23:00:09 -0500

    Added new View Controllers

---
+++ src/BehindTheSite/CompanyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CompanyViewController.swift
+//  BehindTheSite
+//
+//  Created by Nik Khilnani on 2/16/15.
+//  Copyright (c) 2015 Nik Khilnani. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CompanyViewController : UIViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
