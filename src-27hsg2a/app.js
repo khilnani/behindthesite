7314bab29535ad64b1c9480127e87515a546fe3f
Nik <nik@khilnani.org>
2015-03-03 20:19:37 -0500

    Update app.js

--- src-27hsg2a/app.js
+++ src-27hsg2a/app.js
@@ -9,33 +9,6 @@ https://docs.angularjs.org/
 */
 
 //*****************************************************************************
-// Utilities
-
-function trackEvent (product, technology) {
-    console.log('trackEvent: ' + product + ', ' + technology);
-    var title_pre = "BehindTheSite | "
-    
-    if( product != '' && technology == '') {
-      // only product  
-      document.title = title_pre + product;
-      _gaq.push(['_trackEvent', 'Product', product]);
-    } else if( product == '' && technology != '') {
-      // only tech
-      document.title = title_pre + technology;
-      _gaq.push(['_trackEvent', 'Technology', technology]);
-    } else if( product != '' && technology != '') {
-      // both
-      document.title = title_pre + product + " and " + technology;
-      _gaq.push(['_trackEvent', 'Product', product]);
-      _gaq.push(['_trackEvent', 'Technology', technology]);
-    } else if( product == '' && technology == '') {
-      // none
-      document.title = title_pre + "A log of technology stacks";
-    }
-    
-}
-
-//*****************************************************************************
 // App
 
 //var bts = angular.module('bts', ['ngRoute', 'bts.controllers', 'bts.directives', 'bts.filters', 'bts.services','angular-loading-bar', 'ngAnimate', 'ngSanitize' ,'infinite-scroll']);
