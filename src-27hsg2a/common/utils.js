f08e291a5f113c33b5307ac8cf3940cb8fdae8df
Nik <nik@khilnani.org>
2015-03-03 23:16:16 -0500

    Update utils.js

--- src-27hsg2a/common/utils.js
+++ src-27hsg2a/common/utils.js
@@ -6,16 +6,16 @@ function trackEvent (product, technology) {
     if( product != '' && technology == '') {
       // only product  
       document.title = title_pre + product;
-      _gaq.push(['_trackEvent', 'Product', product]);
+      ga('send', 'event', 'Product', product);
     } else if( product == '' && technology != '') {
       // only tech
       document.title = title_pre + technology;
-      _gaq.push(['_trackEvent', 'Technology', technology]);
+      ga('send', 'event', 'Technology', technology);
     } else if( product != '' && technology != '') {
       // both
       document.title = title_pre + product + " | " + technology;
-      _gaq.push(['_trackEvent', 'Product', product]);
-      _gaq.push(['_trackEvent', 'Technology', technology]);
+      ga('send', 'event', 'Product', product);
+      ga('send', 'event', 'Technology', technology);
     } else if( product == '' && technology == '') {
       // none
       document.title = title_pre + "A log of technology stacks";
