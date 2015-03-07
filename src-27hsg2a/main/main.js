bf725b9e523ba8c892bae8dec113ed17a5fdda9d
Nik <nik@khilnani.org>
2015-03-07 17:35:06 -0500

    Update main.js

--- src-27hsg2a/main/main.js
+++ src-27hsg2a/main/main.js
@@ -820,47 +820,16 @@ angular.module('bts.controllers', [])
 
   vm.init();
   
-
- 
-$('#filter-product-typeahead').typeahead({
-  hint: true,
-  highlight: true,
-  minLength: 0
-},
-{
-  name: 'filter-product',
-  displayKey: 'value',
-  source: function(q, cb) {
-    
-    var strs = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
-    var matches, substrRegex;
- 
-    // an array that will be populated with substring matches
-    matches = [];
- 
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
- 
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function(i, str) {
-      if (substrRegex.test(str)) {
-        // the typeahead jQuery plugin expects suggestions to a
-        // JavaScript object, refer to typeahead docs for more info
-        matches.push({ value: str });
-      }
-    });
- 
-    cb(matches);
-  }
-});
+$('#filter-product-typeahead').typeahead({ 
+    minLength:1 
+  }, { 
+    source: 
+      function (query, cb) {
+        cb(['nik']); 
+      } 
+  });
+  
+  
+  
 
 }]);
