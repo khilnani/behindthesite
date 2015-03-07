90c45779f087012c3ad59750d8f5019081da5e30
Nik <nik@khilnani.org>
2015-03-07 12:57:55 -0500

    handle no GA

--- src-27hsg2a/common/utils.js
+++ src-27hsg2a/common/utils.js
@@ -3,21 +3,26 @@ function trackEvent (product, technology) {
     Logger.event('trackEvent: ' + product + ', ' + technology);
     var title_pre = "BehindTheSite | "
     
-    if( product != '' && technology == '') {
-      // only product  
-      document.title = title_pre + product;
-      ga('send', 'event', 'Product', product);
-    } else if( product == '' && technology != '') {
-      // only tech
-      document.title = title_pre + technology;
-      ga('send', 'event', 'Technology', technology);
-    } else if( product != '' && technology != '') {
-      // both
-      document.title = title_pre + product + " | " + technology;
-      ga('send', 'event', 'Product', product);
-      ga('send', 'event', 'Technology', technology);
-    } else if( product == '' && technology == '') {
-      // none
+    try {
+      if( product != '' && technology == '') {
+        // only product  
+        document.title = title_pre + product;
+        ga('send', 'event', 'Product', product);
+      } else if( product == '' && technology != '') {
+        // only tech
+        document.title = title_pre + technology;
+        ga('send', 'event', 'Technology', technology);
+      } else if( product != '' && technology != '') {
+        // both
+        document.title = title_pre + product + " | " + technology;
+        ga('send', 'event', 'Product', product);
+        ga('send', 'event', 'Technology', technology);
+      } else if( product == '' && technology == '') {
+        // none
+        document.title = title_pre + "A log of technology stacks";
+      }
+    } catch(e) {
+      Logger.warn('trackEvent: No GA setup);
       document.title = title_pre + "A log of technology stacks";
     }
     
