3f4700f04b4a97781d6e373c4ea1a30f8a9bf2cb
khilnani <nik@khilnani.org>
2015-03-11 04:34:28 +0000

    added download script o create files from api

---
+++ data/download.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import urllib
+
+urls = {
+    'products': 'http://api.behindthesite.com/v1/products',
+    'used': 'http://api.behindthesite.com/v1/products/used',
+    'stacks': 'http://api.behindthesite.com/v1/stacks',
+    'taxonomy': 'http://api.behindthesite.com/v1/taxonomy'
+  }
+
+for name, link in urls.iteritems():
+  print 'Downloading {1} to {0}'.format(name, link)
+  u = urllib.urlopen( link ) 
+  data = u.read()
+  u.close()
+  
+  f = open(name + '.json', 'w')
+  f.write('{"data":"' + data + '"}')
+  f.close()
