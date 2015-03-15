24d723ad29524a94954ee64a8eaa650fe835c3f6
Nik <nik@khilnani.org>
2015-03-14 23:42:53 -0400

    Update main.js

--- src-27hsg2a/main/main.js
+++ src-27hsg2a/main/main.js
@@ -215,7 +215,7 @@ angular.module('bts.directives', [])
     var ele = angular.element(element);
 
     ele.on('shown.bs.collapse', function (e) {
-      var pos = ele.offset().top - $('#header').height();
+      var pos = ele.offset().top - $('#header').height() - 35; // panel header ht+padding
       Logger.info('shown.bs.collapse: ' + pos );
       $('html, body').animate({
         scrollTop: pos
